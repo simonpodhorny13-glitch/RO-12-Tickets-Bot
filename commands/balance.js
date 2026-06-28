f0cb181dbe2b1fb58a9aab165939c9e4d14e5907
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-06-28 19:44:41 +0200

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,16 +1,46 @@
-const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("balance")
-    .setDescription("Check your current balance"),
+  name: "balance",
+  description: "Check your balance and bookings",
 
-  async execute(interaction, { getUser }) {
-    const user = getUser(interaction.user.id);
+  async execute(interaction) {
+    const data = JSON.parse(fs.readFileSync("./data.json", "utf8"));
+    const userId = interaction.user.id;
 
-    return interaction.reply({
-      content: `💰 Your balance: $${user.balance}`,
-      ephemeral: true,
+    const user = data.users[userId];
+
+    if (!user) {
+      return interaction.reply({
+        content: "💰 Balance: $0\nYou have no bookings yet.",
+        ephemeral: true
+      });
+    }
+
+    const balance = user.balance || 0;
+    const bookings = user.bookings || {};
+
+    let msg = `💰 Balance: $${balance}\n\n🎟️ Bookings:\n`;
+
+    const voyageIds = Object.keys(bookings);
+
+    if (voyageIds.length === 0) {
+      msg += "No active bookings.";
+    } else {
+      for (const id of voyageIds) {
+        const b = bookings[id];
+
+        const typeIcon = b.type === "cabin" ? "🏨 Cabin" : "💺 Seat";
+
+        msg += `\n🚢 Voyage ${id}\n`;
+        msg += `${typeIcon}: ${b.location}\n`;
+        msg += `💰 Paid: $${b.paid}\n`;
+      }
+    }
+
+    interaction.reply({
+      content: msg,
+      ephemeral: true
     });
-  },
+  }
 };
