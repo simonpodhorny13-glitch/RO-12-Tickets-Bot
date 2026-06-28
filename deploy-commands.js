a6317257cf133c9cdaad04c8bc7274090f10cb9c
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-06-28 09:33:00 +0200

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
+
+const commands = [];
+const commandFiles = fs.readdirSync("./commands");
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Deploying slash commands...");
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("Slash commands deployed successfully");
+  } catch (error) {
+    console.error(error);
+  }
+})();
