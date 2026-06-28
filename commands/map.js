1666fd39923af3cfa4e90de87ace66652f6d37c3
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-06-28 19:49:51 +0200

    Update map.js

--- commands/map.js
+++ commands/map.js
@@ -2,43 +2,68 @@ const fs = require("fs");
 
 module.exports = {
   name: "map",
+  description: "View cabin or seat map for a voyage",
 
-  execute(message, args) {
-    const type = args[0];
+  async execute(interaction) {
+    const voyageId = interaction.options.getString("voyage");
+    const type = interaction.options.getString("type"); // cabins or seats
 
-    const filePath = "./cabins.json";
+    const data = JSON.parse(fs.readFileSync("./data.json", "utf8"));
 
-    let cabins = {};
-    if (fs.existsSync(filePath)) {
-      cabins = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    const voyage = data.voyages[voyageId];
+
+    if (!voyage) {
+      return interaction.reply({
+        content: "❌ Voyage not found.",
+        ephemeral: true
+      });
     }
 
-    const isBooked = (c) => cabins[c] ? "X" : " ";
-
-    if (!type || type === "cabins") {
-      return message.reply(`
-🛏️ **CABINS MAP**
-
-ECONOMY
-1A [${isBooked("1A")}]
-1B [${isBooked("1B")}]
-2A [${isBooked("2A")}]
-2B [${isBooked("2B")}]
-
-FIRST
-1C [${isBooked("1C")}]
-1D [${isBooked("1D")}]
-2C [${isBooked("2C")}]
-2D [${isBooked("2D")}]
-
-DOUBLE
-3A [${isBooked("3A")}]
-3B [${isBooked("3B")}]
-3C [${isBooked("3C")}]
-3D [${isBooked("3D")}]
-`);
+    if (!type || (type !== "cabins" && type !== "seats")) {
+      return interaction.reply({
+        content: "❌ Type must be 'cabins' or 'seats'.",
+        ephemeral: true
+      });
     }
 
-    return message.reply("❌ Use: !map cabins");
+    const isCabins = type === "cabins";
+    const map = isCabins ? voyage.cabinMap : voyage.seatMap;
+
+    let output = `🗺️ VOYAGE ${voyageId} ${type.toUpperCase()}\n\n`;
+
+    if (isCabins) {
+      output += `ECONOMY\n`;
+      output += `1A [${map["1A"] ? "X" : " "}]\n`;
+      output += `1B [${map["1B"] ? "X" : " "}]\n`;
+      output += `2A [${map["2A"] ? "X" : " "}]\n`;
+      output += `2B [${map["2B"] ? "X" : " "}]\n\n`;
+
+      output += `FIRST CLASS\n`;
+      output += `1C [${map["1C"] ? "X" : " "}]\n`;
+      output += `1D [${map["1D"] ? "X" : " "}]\n`;
+      output += `2C [${map["2C"] ? "X" : " "}]\n`;
+      output += `2D [${map["2D"] ? "X" : " "}]\n\n`;
+
+      output += `DOUBLE ECONOMY\n`;
+      output += `3A [${map["3A"] ? "X" : " "}]\n`;
+      output += `3B [${map["3B"] ? "X" : " "}]\n`;
+      output += `3C [${map["3C"] ? "X" : " "}]\n`;
+      output += `3D [${map["3D"] ? "X" : " "}]\n`;
+    } else {
+      output += `SEATS\n`;
+      output += `(Simple layout)\n\n`;
+
+      for (let row = 1; row <= 5; row++) {
+        for (let col of ["A", "B", "C", "D", "E"]) {
+          const seat = `${row}${col}`;
+          output += `${seat} [${map[seat] ? "X" : " "}]\n`;
+        }
+      }
+    }
+
+    interaction.reply({
+      content: output,
+      ephemeral: true
+    });
   }
 };
