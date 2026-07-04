1def6fc60ef7d27b2792975a8b80456e83486fe8
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-07-04 10:29:15 +0200

    Update gift.js

--- commands/gift.js
+++ commands/gift.js
@@ -2,48 +2,18 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("gift")
-    .setDescription("Gift cash to another user")
-    .addUserOption(option =>
-      option
-        .setName("user")
-        .setDescription("User to send cash to")
-        .setRequired(true)
-    )
-    .addIntegerOption(option =>
-      option
-        .setName("amount")
-        .setDescription("Amount to send")
-        .setRequired(true)
-    ),
+    .setName("daily")
+    .setDescription("Claim your daily $20 reward"),
 
   async execute(interaction, { getUser }) {
-    const sender = getUser(interaction.user.id);
+    const user = getUser(interaction.user.id);
 
-    const target = interaction.options.getUser("user");
-    const amount = interaction.options.getInteger("amount");
+    const reward = 20;
 
-    if (amount <= 0) {
-      return interaction.reply({
-        content: "❌ Amount must be greater than 0.",
-        ephemeral: true
-      });
-    }
-
-    const receiver = getUser(target.id);
-
-    if (sender.balance < amount) {
-      return interaction.reply({
-        content: "❌ You don’t have enough balance.",
-        ephemeral: true
-      });
-    }
-
-    sender.balance -= amount;
-    receiver.balance += amount;
+    user.balance = Number(user.balance || 0) + reward;
 
     return interaction.reply({
-      content: `🎁 Sent **$${amount}** to **${target.username}**!`,
+      content: `🎁 You claimed your daily reward of **$${reward}**!\n💰 New balance: **$${user.balance}**`,
       ephemeral: true
     });
   }
