31f897eb341c81728676d804a5e7fd22b9ec3368
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-07-04 22:43:53 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,8 @@
 module.exports = {
   name: "ping",
 
+  async execute(message) {
+    const sent = await message.reply("🏓 Calculating ping...");
 
     const latency = sent.createdTimestamp - message.createdTimestamp;
 
