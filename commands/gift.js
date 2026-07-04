e77c2d33d9aa1263c07cbf0b1af570d57d077414
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-07-04 15:28:08 +0200

    Update gift.js

--- commands/gift.js
+++ commands/gift.js
@@ -22,6 +22,39 @@ module.exports = {
     const target = interaction.options.getUser("user");
     const amount = interaction.options.getInteger("amount");
 
+    // Self-gift prevention
+    if (interaction.user.id === target.id) {
+      if (Math.random() < 0.1) {
+        return interaction.reply({
+          content: "Gifting to yourself? Greedy.",
+          ephemeral: true
+        });
+      } else {
+        return interaction.reply({
+          content: "❌️ Cannot gift to yourself.",
+          ephemeral: true
+        });
+      }
+    }
+
+    // Prevent gifting to bots
+    if (target.bot) {
+      // If this bot got gifted
+      if (target.id === interaction.client.user.id) {
+        return interaction.reply({
+          content: "Thanks, this is kind of you but you can't donate to bots like me.",
+          ephemeral: true
+        });
+      } else {
+        // Any other bots
+        return interaction.reply({
+          content: "❌️ Cannot gift to bots.",
+          ephemeral: true
+        });
+      }
+    }
+
+    // 3. Checks if amoumt >0
     if (amount <= 0) {
       return interaction.reply({
         content: "❌ Amount must be greater than 0.",
@@ -29,6 +62,7 @@ module.exports = {
       });
     }
 
+    // Check if enough cash
     if (sender.balance < amount) {
       return interaction.reply({
         content: "❌ You don’t have enough balance.",
@@ -36,6 +70,7 @@ module.exports = {
       });
     }
 
+    // Run transaction
     const receiver = getUser(target.id);
 
     sender.balance -= amount;
