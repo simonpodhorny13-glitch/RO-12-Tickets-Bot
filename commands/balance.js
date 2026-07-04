9794c0cfa991362aa65b9f34f7ef2c48b987f4b1
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-07-04 09:56:25 +0200

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,25 +1,16 @@
 const { SlashCommandBuilder } = require("discord.js");
-const fs = require("fs");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("balance")
     .setDescription("Check your balance"),
 
-  async execute(interaction) {
-    let data = {};
-
-    try {
-      data = JSON.parse(fs.readFileSync("./data.json", "utf8"));
-    } catch (err) {
-      console.log("Data file error:", err);
-    }
-
-    const userId = interaction.user.id;
-    const userBalance = data?.users?.[userId]?.balance ?? 0;
-
+  async execute(interaction, { getUser }) {
     const user = getUser(interaction.user.id);
 
-return interaction.reply({
-  content: `💰 Your balance: $${user.balance}`
-});
+    return interaction.reply({
+      content: `💰 Your balance: ${user.balance}`,
+      ephemeral: true
+    });
+  }
+};
